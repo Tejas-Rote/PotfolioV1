614286261a47f61091e4827fe15317672f6fe919
Tejas Rote <tejas292002@gmail.com>
2023-05-30 14:53:05 +0530

    add: desktop-mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
+import { useEffect } from "react";
 import "./App.css";
 import { Navbar } from "./components/index.js";
 import { Header, Skills, Footer, MyProject } from "./containers/index.js";
 
 const App = () => {
+  useEffect(() => {
+    const isMobile = () => {
+      return window.innerWidth < 768; // Adjust the breakpoint as needed
+    };
+
+    const handleViewportChange = () => {
+      if (isMobile()) {
+        document.documentElement.classList.remove("desktop-mode");
+      } else {
+        document.documentElement.classList.add("desktop-mode");
+      }
+    };
+
+    handleViewportChange(); // Run initially to set the correct mode
+
+    window.addEventListener("resize", handleViewportChange);
+
+    return () => {
+      window.removeEventListener("resize", handleViewportChange);
+    };
+  }, []);
+
   return (
     <div className="app">
       <div className="bg">
